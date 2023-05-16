ac4451708604e69217823b62a1a548c5de0c7d35
jadrover22 <usuari@I01-24.localdomain>
2023-05-16 15:41:54 +0200

    16.05.2023

--- ejercicio303.py
+++ ejercicio303.py
@@ -1,8 +1,13 @@
 import sqlite3
 
 conexion=sqlite3.connect("bd1.db")
-conexion.execute("insert into articulos(descripcion,precio) values (?,?)", ("naranjas", 23.50))
-conexion.execute("insert into articulos(descripcion,precio) values (?,?)", ("peras", 34))
-conexion.execute("insert into articulos(descripcion,precio) values (?,?)", ("bananas", 25))
-conexion.commit()
+try:
+    conexion.execute("""create table articulos (
+                              codigo integer primary key autoincrement,
+                              descripcion text,
+                              precio real
+                        )""")
+    print("se creo la tabla articulos")                        
+except sqlite3.OperationalError:
+    print("La tabla articulos ya existe")                    
 conexion.close()
